c2c1cbe1e90ca5791211db3a5c9bca114356f81e
Shashikant86 <shashikant.jagtap@aol.co.uk>
2014-03-28 19:41:29 +0000

    Added JS Code Coverage Using Istanbul and CSSLint

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,73 +8,38 @@ module.exports = function (grunt) {
     };
 
     // convenience
-    grunt.registerTask('default', ['lint', 'test', 'coverage']);
-    grunt.registerTask('all', ['clean', 'lint', 'test', 'coverage']);
-
-    // continuous integration
-    grunt.registerTask('ci', ['lint', 'qunit:src']);
+    grunt.registerTask('default', ['qunit_junit', 'qunit', 'csslint']);
 
 
     // clean
     grunt.loadNpmTasks('grunt-contrib-clean');
     gruntConfig.clean = {
-        output: ['output']
+        output: ['report']
     };
 
-
-    // lint
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    gruntConfig.jshint = {
-        options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
-            indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true,
-            quotmark: true, regexp: true, undef: true, unused: true, strict: true, trailing: true,
-            maxparams: 3, maxdepth: 2, maxstatements: 50},
-        all: [
-            'Gruntfile.js',
-            'src/js/**/*.js'
-        ]
-    };
     grunt.registerTask('lint', 'jshint');
-
 
     // test
     grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-qunit-istanbul');
     gruntConfig.qunit = {
         src: ['src/test/index.html'],
-        serve: { options: { urls: [ /* specified in test-on-random-port */ ]}}
+        options: {
+        coverage: {
+          src: ['src/js/**/*.js'],
+          instrumentedFiles: 'temp/',
+          htmlReport: 'report/coverage',
+          coberturaReport: 'report/',
+          linesThresholdPct: 20
+        }
+        }
     };
     grunt.loadNpmTasks('grunt-qunit-junit');
     gruntConfig.qunit_junit = {
         options: {
-            dest: 'output/testresults'
+            dest: 'report/'
         }
     };
-
-
-    // serve
-    grunt.registerTask('wait', 'keep running until terminated', function () {
-        /* var done =*/
-        this.async();
-    });
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    gruntConfig.connect = {};
-    gruntConfig.connect.test = {
-        options: {
-            port: 0, // Note: configure connect to choose a randomly available port
-            base: 'src'
-        }
-    };
-    grunt.registerTask('test-on-random-port', function () {
-        grunt.event.once('connect.test.listening', function (host, actualPort) {
-            var url = 'http://localhost:' + actualPort + '/test/index.html';
-            gruntConfig.qunit.serve.options.urls = [url]; // Note: configure qunit to connect to the chosen port
-            grunt.task.run('qunit:serve');
-        });
-
-        grunt.task.run('connect:test');
-    });
-    grunt.registerTask('test', ['qunit_junit', 'test-on-random-port']);
-
 
     // coverage
     grunt.loadNpmTasks('grunt-qunit-cov');
@@ -85,11 +50,30 @@ module.exports = function (grunt) {
             baseDir: 'src',
             srcDir: 'src/js',
             depDirs: ['src/lib', 'src/test'],
-            outDir: 'output/coverage',
+            outDir: 'report/qunit-coverage',
             testFiles: ['src/test/index.html']
         }
     };
     grunt.registerTask('coverage', 'qunit-cov');
+
+    // css lint
+
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    gruntConfig.csslint = {
+      options: {
+    formatters: [
+      {id: 'junit-xml', dest: 'report/csslint_junit.xml'},
+      {id: 'checkstyle-xml', dest: 'report/csscheckstyle.xml'}
+    ]
+  },
+  strict: {
+    options: {
+      import: false
+    },
+    src: ['doc/**/*.css']
+  }
+};
+
 
 
     // grunt
